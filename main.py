8f32a6e39759114a26bfee1d4198e38936e10f98
Kadence Papantonakis <kpapantonakis24@biomedscienceacademy.org>
2020-12-08 14:46:17 +0000

    Finale Draft

--- main.py
+++ main.py
@@ -5,3 +5,27 @@ Period/Core:
 
 
 """
+
+name = input("Enter a Name:")
+color = input("Enter a Color:")
+animal = input("Whats your favoiorit animal:")
+zoo = input("Make up a name for a zoo:")
+wallet = input("An amount of money:")
+shop = input (" You go into a Shop what do you buy:")
+toy = input ("Its a toy:")
+freind = input("Name a freind:")
+car = input("Name a car:")
+
+
+print(name+ "went to the zoo")
+print(name+ " was weareing a " +color)
+print("shirt. They saw a " +animal)
+print("which is there faviorite animal. They were at "+zoo)
+print(freind+ " askes if they can go to the shop.")
+print(name+ " looks at there wallet and sees")
+print(wallet+ " Sure,")
+print(freind+ " they walked in and they buys a")
+print(shop+ " its a toy")
+print(toy+ " .When they leave they see a toy")
+print(car+" .")
+
